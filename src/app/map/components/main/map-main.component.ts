4275712e1ff485aa034c808099f20b234b6f6848
mommao <cnimmana@gmail.com>
2017-05-14 22:51:43 -0400

    Use sidebar in map and style

--- src/app/map/components/main/map-main.component.ts
+++ src/app/map/components/main/map-main.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidebarEntry } from '../../../core/models';
+
 @Component({
   selector: 'app-map-main',
   templateUrl: './map-main.component.html',
   styleUrls: ['./map-main.component.less']
 })
 export class MapMainComponent implements OnInit {
+  sidebarEntries: [SidebarEntry];
 
   constructor() { }
 
   ngOnInit() {
+    this.sidebarEntries = [{
+      name: 'Current',
+      id: 'CURRENT'
+    }, {
+      name: 'Markham',
+      id: 'MARKHAM'
+    }, {
+      name: 'Scarborough',
+      id: 'SCARBOROUGH'
+    }];
   }
 
 }
