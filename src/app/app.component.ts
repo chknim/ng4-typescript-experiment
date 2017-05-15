01939cccc9744d33cfbade18529861e253c45273
mommao <cnimmana@gmail.com>
2017-05-14 20:21:16 -0400

    Wire theme and router to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(
+    public route: ActivatedRoute,
+    public router: Router
+  ) { }
+
+  activateEvent(event) {
+    console.log('Activate Event:', event);
+  }
+
+  deactivateEvent(event) {
+    console.log('Deactivate Event', event);
+  }
 }
