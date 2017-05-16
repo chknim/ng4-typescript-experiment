b9a608005c016e9ce38a6e3bad1676feccef13f1
mommao <cnimmana@gmail.com>
2017-05-16 00:40:52 -0400

    Use sidebar and receive event

--- src/app/map/components/main/map-main.component.ts
+++ src/app/map/components/main/map-main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Injectable, HostBinding } from '@angular/core';
+import { THEME_CONFIG, IThemeConfig } from '../../../../themes/default/theme.config';
 
 import { SidebarEntry } from '../../../core/models';
 
@@ -7,22 +8,40 @@ import { SidebarEntry } from '../../../core/models';
   templateUrl: './map-main.component.html',
   styleUrls: ['./map-main.component.less']
 })
+@Injectable()
 export class MapMainComponent implements OnInit {
-  sidebarEntries: [SidebarEntry];
+  private mapConfig: any;
+  private sidebarEntries: [SidebarEntry];
+  private sidebarBgColor: string;
+  private sidebarTextColor: string;
+  private sidebarSelectBgColor: string;
+  private sidebarSelectTextColor: string;
 
-  constructor() { }
+  constructor(@Inject(THEME_CONFIG) private config: IThemeConfig) {
+    this.mapConfig = config.map;
+  }
 
   ngOnInit() {
     this.sidebarEntries = [{
       name: 'Current',
-      id: 'CURRENT'
+      id: 'CURRENT',
+      icon: 'map-marker'
     }, {
       name: 'Markham',
-      id: 'MARKHAM'
+      id: 'MARKHAM',
+      icon: 'map-marker'
     }, {
       name: 'Scarborough',
-      id: 'SCARBOROUGH'
+      id: 'SCARBOROUGH',
+      icon: 'map-marker'
     }];
+
+    this.sidebarBgColor = this.mapConfig.sidebar.bgColor;
+    this.sidebarTextColor = this.mapConfig.sidebar.textColor;
+    this.sidebarSelectBgColor = this.mapConfig.sidebar.selectBgColor;
+    this.sidebarSelectTextColor = this.mapConfig.sidebar.selectTextColor;
   }
 
+  changeView(id) {
+  }
 }
