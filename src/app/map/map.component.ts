45a6dd24db764b18a774079bfbba178797197aef
mommao <cnimmana@gmail.com>
2017-05-17 21:51:22 -0400

    Check fb session before showing map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,11 @@
+declare const FB: any;
+
 import { Component, OnInit, ViewChild, ComponentFactoryResolver } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { MapMainComponent } from './components/main/map-main.component';
+import { FacebookService } from 'ng2-facebook-sdk';
 
 @Component({
   selector: 'app-map',
@@ -11,7 +16,28 @@ import { MapMainComponent } from './components/main/map-main.component';
   ]
 })
 export class MapComponent implements OnInit {
-  constructor() {}
+  private hasFbConnected = false;
 
-  ngOnInit() {}
+  constructor(
+    private router: Router,
+    private location: Location,
+    private fb: FacebookService) {}
+
+  ngOnInit() {
+    this.fb.getLoginStatus()
+      .then(response => {
+        if (response.status !== 'connected') {
+          this.location.replaceState('/login');
+          this.router.navigateByUrl('/login');
+        } else {
+          this.hasFbConnected = true;
+        }
+      });
+
+    FB.Event.subscribe('auth.authResponseChange', response => {
+      if (response.status === 'connected') {
+        this.hasFbConnected = true;
+      }
+    });
+  }
 }
